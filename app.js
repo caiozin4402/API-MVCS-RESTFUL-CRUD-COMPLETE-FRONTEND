1f9593e8c1e8b2956cfa5b498a82b0ed926b73e6
Caio Dias de Sousa <caiodiasdesousa7@gmail.com>
2025-10-07 16:32:19 -0300

    CasaBranca

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express'); //importa o express
+const app = express(); //recupera uma instancia de express
+const portaServico = 3000;
+
+//inicia a espera por requisições http na porta 3000
+//app.listen(portaServico);
+//console.log("Api rodando no endereço: http://localhost:3000/")
